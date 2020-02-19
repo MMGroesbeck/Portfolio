059dfdbdd1c2482d8c26b8fc13685064ee1502ca
M Groesbeck <m.groesbeck@gmail.com>
2020-02-19 08:53:44 -0800

    data file and images added

---
+++ pageContent.js
@@ -0,0 +1,39 @@
+const pageContent = {
+    about: {
+        title: 'Full Stack Web Developer',
+        contact: [
+            {text: 'm.groesbeck@gmail.com', link: 'mailto:m.groesbeck@gmail.com'},
+            {text: 'GitHub page', link: 'https://github.com/MMGroesbeck'}
+        ],
+        mission: '',
+        text: ''
+    }
+    skills: {
+        intro: 'Student at Lambda School in Full Stack Web Development',
+        items: ['HTML & CSS', 'JavaScript', 'React'],
+        outro: 'Watch this page for further updates.'
+    }
+    projects: {
+        title: 'Projects',
+        items: [
+            {
+                title: 'PicMetric',
+                role: 'UX Developer',
+                tech: 'HTML | CSS',
+                description: 'marketing page for machine-learning-based image-analysis app',
+                deployedLink: 'https://pic-metric.github.io/marketing/',
+                gitHubLink: 'https://github.com/pic-metric/marketing'
+            }
+            {
+                title: 'this page',
+                role: 'Developer',
+                tech: 'HTML | CSS | JavaScript',
+                description: 'early and evolving profile and projects page',
+                deployedLink: '#',
+                gitHubLink: 'https://github.com/mmgroesbeck/portfolio'
+            }
+        ]
+    }
+};
+
+export default pageContent;
