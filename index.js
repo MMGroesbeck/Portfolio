bb38ec16a9765cea1633a751f325aded3aadae55
M Groesbeck <m.groesbeck@gmail.com>
2020-02-19 16:11:58 -0800

    Ready for first deployment.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-const body = document.querySelector("body");
+const aboutBtn = document.querySelector('.about-button');
+const skillsBtn = document.querySelector('.skills-button');
+const projectsBtn = document.querySelector('.projects-button');
+
+const about = document.querySelector('.about-container');
+const skills = document.querySelector('.skills-container');
+const projects = document.querySelector('.projects-wrapper');
+
+aboutBtn.addEventListener('click', ()=>{
+    about.classList.toggle('invisible');
+});
+
+skillsBtn.addEventListener('click', ()=>{
+    skills.classList.toggle('invisible');
+});
+
+projectsBtn.addEventListener('click', ()=>{
+    projects.classList.toggle('invisible');
+});
